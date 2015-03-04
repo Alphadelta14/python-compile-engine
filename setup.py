96acdbaa576aad3ec685ca7229eed578b64ca004
Alpha <alpha@pokesplash.net>
2015-03-04 15:42:22 -0500

    Added setup.py for proper packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='compile-engine',
+      version='2.1',
+      description='Python Compile Engine',
+      url='http://github.com/Alphadelta14/python-compile-engine',
+      author='Alpha',
+      author_email='alpha@projectpokemon.org',
+      license='MIT',
+      packages=['compileengine'],
+      zip_safe=True)
