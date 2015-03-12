6775910e185e13188b929fb9cd5e1d899e08f267
Alpha <alpha@pokesplash.net>
2015-03-12 13:30:15 -0400

    Made Variable name generator a static method

--- compileengine/variable.py
+++ compileengine/variable.py
@@ -8,16 +8,6 @@ OP_PRECEDENCE = [
 ]
 
 
-def name_generator(prefix='local_'):
-    idx = 0
-    while True:
-        # TODO: optimize (full recalculation not needed in loop)
-        name = ''.join([string.ascii_lowercase[int(c, 10)]
-                        for c in oct(idx)[1:]])
-        yield prefix+name
-        idx += 1
-
-
 class Variable(object):
     def __init__(self, base=None, value=None):
         self.value = value
@@ -81,3 +71,13 @@ class Variable(object):
 
     def __rshift__(self, other):
         return self.operate(operator.rshift, other)
+
+    @staticmethod
+    def name_generator(prefix='local_'):
+        idx = 0
+        while True:
+            # TODO: optimize (full recalculation not needed in loop)
+            name = ''.join([string.ascii_lowercase[int(c, 10)]
+                            for c in oct(idx)[1:]])
+            yield prefix+name
+            idx += 1
